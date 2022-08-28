c460a2967b9e75bd8f640247a55b341a3894f98e
Michelle <michelle.douma@gmail.com>
2022-08-28 18:30:09 -0400

    testing routes

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -6,7 +6,7 @@ const {
     createPizza,
     updatePizza,
     deletePizza
-} = require ('./controller/pizza-controller');
+} = require ('../../controllers/pizza-controller');
 
 //Set up GET all and POST at /api/pizzas
 router
